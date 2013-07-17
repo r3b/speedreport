cea54dc4c83c8e869bf8b972bb263bf6c0dcfc1e
ryan bridges <ryan.bridges@turner.com>
2013-07-16 23:10:11 -0400

    trimming down the CSS HREF to a filename only and putting it in side the slice at the same angle.

--- core/cssCheck.js
+++ core/cssCheck.js
@@ -1,27 +1,37 @@
 function run(params, callback){
-    var page=page = require('webpage').create();
+    var page = require('webpage').create();
     page.onConsoleMessage = function (msg) {console.log(msg);};
     page.open(params.url, function(status) {
         var pageInfo=page.evaluate(function(){
             var sheetCount=0,ruleCount=0, unmatchedRuleCount=0, unmatched=[], matched=[], matches;
             Array.prototype.slice.apply(document.styleSheets).forEach(function(styleSheet){
                 if(styleSheet.cssRules){
-                    Array.prototype.slice.apply(styleSheet.cssRules).forEach(function(rule){
-                        matches=document.querySelectorAll(rule.selectorText);
-                        if(matches && matches.length){
-                            matched.push({rule:rule.selectorText, matches:matches.length||0, stylesheet:styleSheet.href||'embedded'})
-                        }else{
-                            unmatchedRuleCount++;
-                            unmatched.push({rule:rule.selectorText, matches:0, stylesheet:styleSheet.href||'embedded'})
-                        }
-                        ruleCount++;
-                    })
+                    try{
+                        Array.prototype.slice.apply(styleSheet.cssRules).forEach(function(rule){
+                            matches=document.querySelectorAll(rule.selectorText);
+                            //console.log(rule.selectorText, matches);
+                            try{
+                                if(matches && matches.length){
+                                    matched.push({rule:rule.selectorText, matches:matches.length||0, stylesheet:styleSheet.href||'embedded'})
+                                }else{
+                                    unmatchedRuleCount++;
+                                    unmatched.push({rule:rule.selectorText, matches:0, stylesheet:styleSheet.href||'embedded'})
+                                }
+                            }catch(e){
+                                console.error('<!--', rule.selectorText, e, '-->');
+                            }
+                            ruleCount++;
+                        })
+                    }catch(e1){
+                        console.error('<!--', styleSheet.href, e1, '-->');
+                    }
                 }
                 sheetCount++;
             });
             return {sheets:sheetCount,rules:ruleCount, unmatched:unmatched, matched:matched}
         });
         data=JSON.stringify(pageInfo, undefined, 4)
+        pageInfo
         //console.log(data);
         formatted=format('templates/cssCheck.html', {DATA:data});
         //console.log(formatted);
